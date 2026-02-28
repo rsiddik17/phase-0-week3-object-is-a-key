9cdcdc29fe67cb9dd5afba3964414022e73ddb53
rsiddik17 <rsiddik369@gmail.com>
2026-02-28 19:24:22 +0700

    part1 done

---
+++ quiz/part1/soal5.js
@@ -0,0 +1,25 @@
+//sistem ubah hurufnya misal huruf a diubah menjadi b, c menjadi d, b menjadi c, z menjadi a
+//intinya ubah huruf menjadi huruf setelahnya
+function ubahHuruf(kata) {
+  // you can only write your code here!
+  let result = "";
+
+  for(let i = 0; i < kata.length; i++) {
+    let char = kata.charCodeAt(i);
+
+    if(kata[i] === "z") {
+        result += "a";
+    } else {
+        result += String.fromCharCode(char + 1);
+    }
+  }
+
+  return result;
+}
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
